66b7b7324561edc6a54dd69580047b0e67b3cf7d
juandavid.pineda <juandavid.pineda@globant.com>
2022-01-23 14:28:34 -0500

    tests

---
+++ app/src/test/java/com/juanpineda/mymovies/ui/main/MainIntegrationTests.kt
@@ -0,0 +1,64 @@
+package com.juanpineda.mymovies.ui.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.juanpineda.data.source.LocalDataSource
+import com.juanpineda.mymovies.FakeLocalDataSource
+import com.juanpineda.mymovies.defaultFakeMovies
+import com.juanpineda.mymovies.initMockedDi
+import com.juanpineda.mymovies.testshared.mockedMovie
+import com.juanpineda.usecases.GetPopularMovies
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MainIntegrationTests : AutoCloseKoinTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var observer: Observer<MainViewModel.UiModel>
+
+    private lateinit var vm: MainViewModel
+
+    @Before
+    fun setUp() {
+        val vmModule = module {
+            factory { MainViewModel(get(), get()) }
+            factory { GetPopularMovies(get()) }
+        }
+
+        initMockedDi(vmModule)
+        vm = get()
+    }
+
+    @Test
+    fun `data is loaded from server when local source is empty`() {
+        vm.model.observeForever(observer)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(observer).onChanged(MainViewModel.UiModel.Content(defaultFakeMovies))
+    }
+
+    @Test
+    fun `data is loaded from local source when available`() {
+        val fakeLocalMovies = listOf(mockedMovie.copy(10), mockedMovie.copy(11))
+        val localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = fakeLocalMovies
+        vm.model.observeForever(observer)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(observer).onChanged(MainViewModel.UiModel.Content(fakeLocalMovies))
+    }
+}
---
+++ testShared/src/main/java/com/juanpineda/mymovies/testshared/mocks.kt
@@ -0,0 +1,17 @@
+package com.juanpineda.mymovies.testshared
+
+import com.juanpineda.domain.Movie
+
+val mockedMovie = Movie(
+    0,
+    "Title",
+    "Overview",
+    "01/01/2025",
+    "",
+    "",
+    "EN",
+    "Title",
+    5.0,
+    5.1,
+    false
+)
